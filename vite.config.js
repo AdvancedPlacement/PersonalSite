62af7c874857535b7375aceea9e71c4aca44cb5b
Jason W <jfrostwells@gmail.com>
2026-07-14 22:44:16 -0700

    vercel 2

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,15 @@ import {defineConfig} from 'vite'
 
 export default defineConfig({
   build: {
-    target: 'esnext' //browsers can handle the latest ES features
+    target: 'esnext', //browsers can handle the latest ES features
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        projects: 'projects.html',
+        contact: 'contact.html'
+        // add one line per page you have, e.g.:
+        // about: 'about.html',
+      },
+    },
   }
-})
+}) 
